bb480ac1176de4bcf279a4214349f26dac34077a
Marco Amato <marco@MacBook-Air-di-Marco.local>
2015-01-04 17:20:12 +0100

    Update tabletable and create demo video

---
+++ exampleOfUse/tabletable/README.md
@@ -0,0 +1,29 @@
+*TABLETABLE is about linearize table...*
+
+- Alpha version. Please use and report any issue :)
+
+
+A query plugin that instant linearize table css for mobile device
+
+Use select the table and launch tabletable :
+
+$('.tableClass').tabletable();
+
+example:
+
+<script>
+        $( document ).ready(function() {
+            $('.mytable2').tabletable();
+            $('.mytable1').tabletable();
+            $('.mytable').tabletable();
+        });
+</script>
+
+
+That's it!
+
+Check it on :
+http://www.ottob.it/share/tabletable/tabletable.html
+
+
+
---
+++ exampleOfUse/tabletable/scripts/jquery.tabletable.js
@@ -0,0 +1,79 @@
+/**
+ * Created by Marco Amato on 19/07/14.
+ */
+
+$.fn.tabletable = function() {
+
+    var _tabletable = this;
+
+    function getWindowDim() {
+        var dim = {
+            width: 0, height: 0
+        };
+        if (document.body && document.body.offsetWidth) {
+            dim.width = document.body.offsetWidth;
+            dim.height = document.body.offsetHeight;
+
+        }
+        if (document.compatMode == 'CSS1Compat' && document.documentElement && document.documentElement.offsetWidth) {
+            dim.width = document.documentElement.offsetWidth;
+            dim.height = document.documentElement.offsetHeight;
+
+        }
+        if (window.innerWidth && window.innerHeight) {
+            dim.width = window.innerWidth;
+            dim.height = window.innerHeight;
+
+        }
+        return dim;
+    }
+
+
+    var titles = [];
+
+    _tabletable.find('tr').eq(0).find('th').each(function(i){
+          titles[i] = $(this).html();
+    });
+
+    _tabletable.find('tr').each(function(_i){
+
+        if(_i == 0){
+            $(this).find('th').each(function(i){
+                titles[i] = $(this).html();
+            });
+        }else{
+            $(this).find('td').each(function(i){
+                $(this).attr('data-title',titles[i]);
+            });
+        }
+    });
+
+
+      $(window).on('orientationchange', function(e) {
+        if(getWindowDim().width > getWindowDim().height ){
+            _tabletable.removeClass('linearize');
+         }else{
+            _tabletable.addClass('linearize');
+         }
+      }).trigger('orientationchange');;
+
+    /*
+    if (_tabletable.width() > (getWindowDim().width -20) && (!_tabletable.hasClass('linearize'))){
+        _tabletable.addClass('linearize');
+    }else{
+        _tabletable.removeClass('linearize');
+    }
+*/
+    /*
+    if(!_tabletable.hasClass('tabletable')){
+        $( window ).on( "orientationchange", function( event ) {
+
+            _tabletable.tabletable();
+        });
+        _tabletable.addClass('tabletable');
+    }
+    */
+
+
+    return this;
+}
